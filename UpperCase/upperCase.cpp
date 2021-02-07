53d938a5fef2b76f1b5d2a71062a43eccfbcb922
Aruj Mahajan <arujmahajan@Arujs-MacBook-Pro.local>
2021-02-07 17:02:02 +0530

    upper case

---
+++ UpperCase/upperCase.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char lowerCase, upperCase;
+    int offset;
+
+    cout<<"Please enter the lower case letter "<<endl;
+    cin>>lowerCase;
+    offset = (int) (lowerCase - 'a');
+    upperCase = (char) ('A' +  offset);
+    cout<<"The upper case of "<<lowerCase<<" is "<<upperCase<<endl;
+    
+    return 0;
+
+}
