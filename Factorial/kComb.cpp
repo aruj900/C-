8e7613fe91333725d8e37564fe33f28b9c72fd48
Aruj Mahajan <arujmahajan@Arujs-MacBook-Pro.local>
2021-02-08 14:52:45 +0530

    using 2 func

--- Factorial/kComb.cpp
+++ Factorial/kComb.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+int factorial(int num);
+int kComb(int n, int k);
+
+int main(){
+    int n, k, k_comb;
+    
+
+    cout<<"Please enter n and k"<<endl;
+    cin>>n>>k;
+    k_comb = kComb(n,k);
+    cout<<n<<" choose "<<k<<" is "<<k_comb<<endl;
+    return 0;
+    
+}
+
+int kComb(int n, int k){
+    int nFact, kFact, n_kFact;
+    nFact = factorial(n);
+    kFact = factorial(k);
+    n_kFact = factorial(n - k);
+
+    return (nFact /(kFact * n_kFact));
+
+}
+
 int factorial(int num){
     int fact, i;
     fact = 1;
@@ -8,22 +34,5 @@ int factorial(int num){
         fact *= i;
     }
     return fact;
-}
-
-int main(){
-    int n, k, k_comb;
-    int nFact, kFact, n_kFact;
-
-    cout<<"Please enter n and k"<<endl;
-    cin>>n>>k;
-
-    nFact = factorial(n);
-    kFact = factorial(k);
-    n_kFact = factorial(n - k);
-
-    k_comb = nFact /(kFact * n_kFact);
-    cout<<n<<" choose "<<k<<" is "<<k_comb<<endl;
-    return 0;
-    
 }
 
