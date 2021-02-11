de7e5b8269dd38481633cd3546fc811d14c15a73
Aruj Mahajan <arujmahajan@Arujs-MacBook-Pro.local>
2021-02-11 18:26:12 +0530

    even

---
+++ AreEven/even.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+bool areEven(int arr[], int n);
+int main(){
+    int arr[4] = {2,34,24,12};
+    if(areEven(arr,4)){
+        cout<<"All are even"<<endl;
+    }else
+        cout<<"All are not evrn"<<endl;
+    return 0;
+
+}
+
+
+bool areEven(int arr[], int n){
+    bool res;
+    if (n == 1){
+        return (arr[0] % 2 == 0);
+    }
+    else{
+        res = areEven(arr,n-1);
+        if (res == true){
+            return (arr[n-1] % 2 == 0);
+            
+
+        }else{
+            return false;
+        }
+
+    }
+
+}
