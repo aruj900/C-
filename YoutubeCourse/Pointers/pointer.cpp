21d2cbad5e7c6b37768c9b728dfe19819cfb8119
Aruj Mahajan <arujmahajan@Arujs-MacBook-Pro.local>
2021-02-27 16:57:10 +0530

    pointers

---
+++ YoutubeCourse/Pointers/pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int a = 3;
+    int* b = &a;
+    cout<<b<<endl;
+    cout<<&a<<endl;
+    cout<<"Value at address b is "<<*b<<endl;
+    
+    // & is address of operator
+
+    // * is Dereference operator
+
+
+}
